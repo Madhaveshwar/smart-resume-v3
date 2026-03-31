f1ad8be1ea909f3a6b6725faae1dc03e8dc27686
Madhaveshwar <madhavnimmagadda@gmail.com>
2026-03-31 22:55:44 +0530

    final fix root endpoint

--- backend/src/main/java/com/smartresume/SmartResumeApplication.java
+++ backend/src/main/java/com/smartresume/SmartResumeApplication.java
@@ -3,7 +3,7 @@ package com.smartresume;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.smartresume")
 public class SmartResumeApplication {
     public static void main(String[] args) {
         SpringApplication.run(SmartResumeApplication.class, args);
